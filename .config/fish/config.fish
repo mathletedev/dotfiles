26e00fca394c58d582fb6e0b22c2691fde8c047b
Delta Eames <mathletedev@gmail.com>
2021-12-27 13:54:30 -0800

    say instead of think

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,6 @@ alias ls="exa -a --long --git"
 alias dfs="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 alias clean="clear; neofetch"
 alias thought="fortune | pokemonthink"
-alias strat="aow | pokemonthink"
+alias strat="aow | pokemonsay"
 
 starship init fish | source
