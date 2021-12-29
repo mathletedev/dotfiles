1e5295ef043417bcdb8269c8b685ad69447fadbe
Delta Eames <mathletedev@gmail.com>
2021-12-29 14:12:14 -0800

    tty clock

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ set -g theme_color_scheme nord
 alias ls="exa -a --long --git"
 alias dfs="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 alias clean="clear; neofetch"
+alias clock="tty-clock -c"
 alias thought="fortune | pokemonthink"
 alias strat="aow | pokemonsay"
 
