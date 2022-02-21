1473729c8aac08fdc8a0c8973243d659228d95b6
Delta Eames <mathletedev@gmail.com>
2022-02-21 01:08:24 -0800

    dmenu rice

---
+++ .config/dmenu/config.h
@@ -0,0 +1,15 @@
+static int topbar = 1;
+
+static const char *fonts[] = {"Ubuntu:size=10"};
+
+static const char *prompt = "";
+
+static const char *colors[SchemeLast][2] = {
+    [SchemeNorm] = {"#dcdfe4", "#282c34"},
+    [SchemeSel] = {"#61afef", "#282c34"}};
+
+static unsigned int lines = 20;
+
+static const char worddelimiters[] = " ";
+
+static const unsigned int border_width = 5;
