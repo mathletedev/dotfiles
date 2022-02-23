e0c536f66293738417efba48f46332c76d30afbe
Delta Eames <mathletedev@gmail.com>
2022-02-22 22:14:54 -0800

    more red and blue

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,8 @@ set -g theme_color_scheme nord
 alias ls="exa -a --long --git"
 alias dfs="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 alias clean="clear; neofetch"
-alias clock="tty-clock -c"
+alias cmatrix="cmatrix -C blue"
+alias clock="tty-clock -c -C 1"
 alias thought="fortune | pokemonthink"
 alias strat="aow | pokemonsay"
 
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -85,10 +85,10 @@ main = do
         handleEventHook = handleEventHook def <+> docksEventHook,
         logHook = dynamicLogWithPP $ xmobarPP {
             ppOutput = hPutStrLn xmproc,
-            ppCurrent = xmobarColor "#61afef" "" . wrap "<box type=Bottom width=2 mb=2>" "</box>",
-            ppHidden = xmobarColor "#c678dd" "" . wrap "<box type=Bottom width=2 mb=2>" "</box>",
-            ppHiddenNoWindows = xmobarColor "#c678dd" "",
-            ppUrgent = xmobarColor "#e06c75" "" . wrap "<box type=Bottom width=2 mb=2>" "</box>",
+            ppCurrent = xmobarColor "#e06c75" "" . wrap "<box type=Bottom width=2 mb=2>" "</box>",
+            ppHidden = xmobarColor "#61afef" "" . wrap "<box type=Bottom width=2 mb=2>" "</box>",
+            ppHiddenNoWindows = xmobarColor "#61afef" "",
+            ppUrgent = xmobarColor "#c678dd" "" . wrap "<box type=Bottom width=2 mb=2>" "</box>",
             ppTitle = xmobarColor "#dcdfe4" "",
             ppLayout = xmobarColor "#56b6c2" "" . myLayoutPrinter,
             ppSep = "  "
