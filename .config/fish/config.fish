57972057006b9d2bc54b89a58892ea98f001b3ec
Delta Eames <mathletedev@gmail.com>
2022-02-28 00:05:03 -0800

    nav back abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ set PATH ~/bin/ $PATH
 set -gx EDITOR nvim
 set -g theme_color_scheme nord
 
+abbr -a -- - "cd -"
+
 alias ls="exa -a --long --git"
 alias dfs="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 alias clean="clear; neofetch"
