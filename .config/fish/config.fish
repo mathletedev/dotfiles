3dd882f4708b50203cf40cb37245d371d8035cbf
Neo Delta <mathletedev@gmail.com>
2022-05-15 18:15:31 -0700

    spawn new kitty instance at same directory

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set -gx EDITOR nvim
 set -g theme_color_scheme nord
 
 abbr -a -- - "cd -"
+abbr -a -- n "kitty &"
 
 alias ls="exa -a --long --git"
 alias dfs="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
