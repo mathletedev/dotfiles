76b9bff85572e6c885a063bdff1705e3724b1839
Neo Delta <mathletedev@gmail.com>
2022-06-05 14:41:02 -0700

    dmenu colors

--- .config/dmenu/config.h
+++ .config/dmenu/config.h
@@ -1,15 +1,11 @@
 static int topbar = 1;
-
-static const char *fonts[] = {"Ubuntu:size=10"};
-
+static const char *fonts[] = { "Ubuntu:size=10" };
 static const char *prompt = "";
-
 static const char *colors[SchemeLast][2] = {
-    [SchemeNorm] = {"#dcdfe4", "#282c34"},
-    [SchemeSel] = {"#61afef", "#282c34"}};
+	[SchemeNorm] = { "#cdd6f4", "#1e1e2e" },
+	[SchemeSel] = { "#89b4fa", "#1e1e2e" }
+};
 
 static unsigned int lines = 20;
-
 static const char worddelimiters[] = " ";
-
 static const unsigned int border_width = 5;
