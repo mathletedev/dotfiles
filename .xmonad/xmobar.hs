14d760102142db36056df54b59ec57027fdd8610
Neo Delta <mathletedev@gmail.com>
2022-06-30 22:15:54 -0700

    xmobar refactoring

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -8,10 +8,13 @@ Config {
 	iconRoot = ".xmonad/icons/",
 	commands = [
 		Run StdinReader,
-		Run Date "<fc=#89b4fa><fn=1>\xf017</fn>  %H:%M:%S</fc>" "time" 10,
-		Run Cpu ["-t", "<fc=#f38ba8><fn=1>\xf2db</fn>  <total>%</fc>"] 20,
-		Run Memory ["-t", "<fc=#fab387><fn=1>\xf538</fn>  <usedratio>%</fc>"] 20,
-		Run Alsa "default" "Master" ["-t", "<fc=#f9e2af><fn=1><status></fn>  <volume>%</fc>", "--", "-O", "\xf028", "-o", "\xf6a9", "-C", "#f9e2af", "-c", "#f9e2af"],
+		Run Date "<action=`kitty -e tty-clock -c`><fc=#89b4fa><fn=1>\xf017</fn>  %H:%M:%S</fc></action>" "time" 10,
+		Run Cpu ["-t", "<action=`kitty -e htop`><fc=#f38ba8><fn=1>\xf2db</fn>  <total>%</fc></action>"] 20,
+		Run Memory ["-t", "<action=`kitty -e htop`><fc=#fab387><fn=1>\xf538</fn>  <usedratio>%</fc></action>"] 20,
+		Run Alsa "default" "Master" [
+			"-t", "<action=`kitty -e alsamixer`><fc=#f9e2af><fn=1><status></fn>  <volume>%</fc></action>",
+			"--", "-O", "\xf028", "-o", "\xf6a9", "-C", "#f9e2af", "-c", "#f9e2af"
+		],
 		Run WeatherX "KPUW" [
 			("clear", "\xf185"),
 			("sunny", "\xf185"),
@@ -26,10 +29,10 @@ Config {
 			("overcast", "\xf73d"),
 			("obscured", "\xf75f")
 		] ["-t", "<fc=#a6e3a1><fn=1>\xf2c9</fn>  <tempC>°C  <fn=1><skyConditionS></fn></fc>"] 18000,
-		Run Date "<fc=#89b4fa><fn=1>\xf133</fn>  %m/%d/%Y</fc>" "date" 36000,
-		Run DynNetwork ["-t", "<fc=#cba6f7><fn=1>\xf019</fn>  <rx> KB/s  <fn=1>\xf7c0</fn>  <tx> KB/s</fc>"] 20
+		Run Date "<action=`kitty --hold -e cal -y`><fc=#89b4fa><fn=1>\xf133</fn>  %m/%d/%Y</fc></action>" "date" 36000,
+		Run DynNetwork ["-t", "<action=`kitty -e nmtui`><fc=#cba6f7><fn=1>\xf019</fn>  <rx> KB/s  <fn=1>\xf7c0</fn>  <tx> KB/s</fc></action>"] 20
 	],
 	sepChar = "%",
 	alignSep = "}{",
-	template = "  <action='dmenu_run'><icon=sharingan.xpm/></action>  %StdinReader%}<action='kitty -e tty-clock -c'>%time%</action>{<action='kitty -e htop'>%cpu%</action>    %memory%    <action='kitty -e cava'>%alsa:default:Master%</action>    %KPUW%    <action='kitty --hold -e cal -y'>%date%</action>    <action='kitty -e nmtui'>%dynnetwork%</action>    "
+	template = "  <action=`dmenu_run`><icon=sharingan.xpm/></action>  %StdinReader%}%time%{%cpu%    %memory%    %alsa:default:Master%    %KPUW%    %date%    %dynnetwork%    "
 }
