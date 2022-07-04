bc6f98f39cfc93b30cf12b78a68f3fdf37522a4a
Neo Delta <mathletedev@gmail.com>
2022-07-04 01:37:00 -0700

    remove clean command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,6 @@ abbr -a -- n "kitty &"
 
 alias ls="exa -a --long --git"
 alias dfs="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
-alias clean="clear; neofetch"
 alias air="~/go/bin/air"
 alias matrix="unimatrix -af -s 96"
 alias clock="tty-clock -c"
